63ba22f022acb29630e406927486049dbc02a186
chocolaggibbiddori <didrkd684@naver.com>
2023-01-17 23:00:26 +0900

    test : 스프링 AOP 구현 5
    어드바이스 순서 - @Order : Aspect 단위로 적용 가능

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,8 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV3;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -15,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired
