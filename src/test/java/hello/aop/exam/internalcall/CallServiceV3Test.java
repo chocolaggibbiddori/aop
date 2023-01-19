bdb290c3a6bd451e9f42b7d5ca27dce53b6ea601
chocolaggibbiddori <didrkd684@naver.com>
2023-01-19 12:31:07 +0900

    test : 스프링 AOP 주의사항 - 프록시와 내부 호출
    대안 3 구조 변경

---
+++ src/test/java/hello/aop/exam/internalcall/CallServiceV3Test.java
@@ -0,0 +1,22 @@
+package hello.aop.exam.internalcall;
+
+import hello.aop.exam.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV3Test {
+
+    @Autowired
+    CallServiceV3 service;
+
+    @Test
+    void external() {
+        service.external();
+    }
+}
